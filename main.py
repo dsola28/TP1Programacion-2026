5872b0da75aaf08f5a1e205406b6b72d6d4836ac
dsola28 <dsola@uade.edu.ar>
2026-08-18 15:43:01 -0300

    desarrollo main iniciado

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-def main():
-    print("Hello, World!")
+def admin():
+    return
 
+def reserva():
+    return
+
+
+
+def main():
+    res = input("Bienvenedido a A la mesa!, desea realizar una reserva? (si/no)(en caso de ser administrador, ingrese el código de acceso): "))
+    #código admin: 1010
+    if res == "si":
+        print("Perfecto, vamos a realizar la reserva.")
+        # función reserva
+    elif res == "1010":
+        print("Bienvenido administrador, puede acceder a las funciones de administración.")
+        # función admin
+    elif res == "no":
+        print("Gracias por su visita, esperamos verlo pronto.")
 
 main()
